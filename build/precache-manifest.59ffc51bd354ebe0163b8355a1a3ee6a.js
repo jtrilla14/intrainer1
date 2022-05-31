b527483f9c133e8a55545c561640c28d48056813
jtrilla14 <95723032+jtrilla14@users.noreply.github.com>
2022-05-31 16:53:11 -0500

    Add files via upload
    build

---
+++ build/precache-manifest.59ffc51bd354ebe0163b8355a1a3ee6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c552fa871ba553c5f4c857eb934469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7049016ea83150e11877",
+    "url": "/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "008d617d363f70b1c1b9",
+    "url": "/static/js/2.7278d14a.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.7278d14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7049016ea83150e11877",
+    "url": "/static/js/main.95947cf8.chunk.js"
+  },
+  {
+    "revision": "88a2a38fad697d24ce4c",
+    "url": "/static/js/runtime-main.abf55560.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+importScripts("/precache-manifest.59ffc51bd354ebe0163b8355a1a3ee6a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.skipWaiting()
+
+workbox.routing.registerRoute(
+  new RegExp('https:.*min\.(css|js)'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'cdn-cache'
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.json'),
+  new workbox.strategies.NetworkFirst()
+)
+
+self.addEventListener('fetch', event => {
+  if(event.request.method === 'POST' || event.request.method === 'DELETE') {
+    event.respondWith(
+      fetch(event.request).catch(err => {
+        return new Response(
+          JSON.stringify({ error: 'This action disabled while app is offline' }), {
+            headers: { 'Content-Type': 'application/json' }
+          }
+        )
+      })
+    )
+  }
+})
+
+self.addEventListener('push', event => {
+  event.waitUntil(self.registration.showNotification('Mi PWA', {
+    icon: '/logo192.png',
+    body: event.data.text()
+  }))
+})
+
+// Comentar para prevenir que la PWA guarde cache
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
